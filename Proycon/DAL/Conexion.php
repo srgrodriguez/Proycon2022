4bd22737d85bff671585dd49bdaaeca4ce653d3d
Steven <srgrodriguez27@gmail.com>
2022-11-16 13:18:50 -0600

    Se ajusta archivo

--- Proycon/DAL/Conexion.php
+++ Proycon/DAL/Conexion.php
@@ -1,18 +1,23 @@
 <?php
 
-class Conexion {
+class Conexion
+{
 
-    function CrearConexion() {
-        $connect = new mysqli("localhost", "proycon_BD", "Pr0yc0n+", "proycon_BODEGA");
-        //$connect = new mysqli("localhost", "root", "", "proycon_bodega");
-        if($connect->connect_error || $connect->error ){
-          echo "<script>alert('Error de Conexion con la base de datos ".
-                  $connect->connect_error." ERROR ".$connect->error."')</script>";
-           exit();      
-        }
-        else{
-        return $connect;
+    function CrearConexion()
+    {
+        $dominio = $_SERVER['HTTP_HOST'];
+        $connect = null;
+        if ($dominio == 'localhost')
+            $connect = new mysqli("localhost", "root", "", "proycon_bodega");
+        else
+            $connect = new mysqli("localhost", "proycon_BD", "Pr0yc0n+", "proycon_BODEGA");
+
+        if ($connect->connect_error || $connect->error) {
+            echo "<script>alert('Error de Conexion con la base de datos " .
+                $connect->connect_error . " ERROR " . $connect->error . "')</script>";
+            exit();
+        } else {
+            return $connect;
         }
     }
-
 }
