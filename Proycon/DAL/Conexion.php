a175cceb691eb3c2ec2c19c56fe557d9554e40ae
Steven <srgrodriguez27@gmail.com>
2022-11-16 20:16:39 -0600

    Se agrega el manejo de errores

--- Proycon/DAL/Conexion.php
+++ Proycon/DAL/Conexion.php
@@ -1,23 +1,28 @@
 <?php
+require_once 'Log.php';
 
 class Conexion
 {
 
     function CrearConexion()
     {
-        $dominio = $_SERVER['HTTP_HOST'];
-        $connect = null;
-        if ($dominio == 'localhost')
-            $connect = new mysqli("localhost", "root", "", "proycon_bodega");
-        else
-            $connect = new mysqli("localhost", "proycon_BD", "Pr0yc0n+", "proycon_BODEGA");
+        try {
 
-        if ($connect->connect_error || $connect->error) {
-            echo "<script>alert('Error de Conexion con la base de datos " .
-                $connect->connect_error . " ERROR " . $connect->error . "')</script>";
-            exit();
-        } else {
-            return $connect;
+            $connect = null;
+            if (EsProduccion())
+                $connect = new mysqli("localhost", "proycon_BD", "Pr0yc0n+", "proycon_BODEGA");
+            else
+                $connect = new mysqli("localhost", "root", "", "proycon_bodega");
+
+            if ($connect->connect_error || $connect->error) {
+                $error = "Error de Conexion con la base de datos " . $connect->connect_error . " ERROR " . $connect->error;
+                Log::GenerarArchivoLog($error);
+                exit();
+            } else {
+                return $connect;
+            }
+        } catch (Exception  $e) {
+            throw $e;
         }
     }
 }
